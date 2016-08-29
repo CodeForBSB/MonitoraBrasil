a779cd325180089c12ecf8ed50bf138647f431cd
geraugu <geraldo.morais@gmail.com>
2016-08-29 17:27:03 +0000

    first commit to rest service

---
+++ rest/Monitora.js
@@ -0,0 +1,351 @@
+'use strict';
+
+var Parse = require('parse/node');
+var format=require('format-number');
+
+
+var Monitora = {
+	/*
+	Build element for project
+	*/
+	buildElementProject: function(project){
+		var mReturn = {
+            title: project.get("tx_nome"),
+            subtitle: project.get("nome_autor")+"\n"+project.get("txt_ementa"),
+            //item_url: "https://www.oculus.com/en-us/rift/",               
+            //image_url: "http://www.camara.gov.br/internet/deputado/bandep/"+congressman.get("idCadastro")+".jpg",
+            buttons: [ {
+              type: "postback",
+              title: "Ver ementa",
+              payload: "{\"id_project\": \""+project.id+"\",\"nome\": \""+project.get("tx_nome")+"\", \"type\": \"project_ementa\"}",
+            }],
+          };
+		  return mReturn;
+	},
+	
+	/*
+	  Build element for congressman with options: Spends, assiduity and projects
+	*/
+	buildElements:  function(congressman){
+		var mReturn = {
+				title: congressman.get("nome"),
+				subtitle: congressman.get("siglaPartido")+" - "+congressman.get("uf")+"\n"+congressman.get("telefone"),
+				//item_url: "https://www.oculus.com/en-us/rift/",               
+				image_url: "http://www.camara.gov.br/internet/deputado/bandep/"+congressman.get("idCadastro")+".jpg",
+				buttons: [ {
+				  type: "postback",
+				  title: "Gastos - cota parlamentar",
+				  payload: "{\"id_politico\": \""+congressman.id+"\","+
+					"\"nome\": \""+congressman.get("nome")+"\","+
+					"\"gender\": \""+congressman.get("sexo")+"\","+
+					"\"type\": \"gastos_cota\"}",
+				},{
+				  type: "postback",
+				  title: "Presença",
+				  payload: "{\"id_politico\": \""+congressman.id+"\","+
+					"\"nome\": \""+congressman.get("nome")+"\","+
+					"\"gender\": \""+congressman.get("sexo")+"\","+ 
+					"\"type\": \"presenca\"}",
+				},{
+				  type: "postback",
+				  title: "Projetos",
+				  payload: "{\"id_politico\": \""+congressman.id+"\","+
+					"\"nome\": \""+congressman.get("nome")+"\","+
+					"\"gender\": \""+congressman.get("sexo")+"\","+ 
+					"\"type\": \"projetos\"}",
+				}],
+			  };
+			  return mReturn;
+	}, 
+	
+	/*
+	Search a congressman by name
+	*/
+	getPolitico: function(params, callback){
+		var Politico = Parse.Object.extend("Politico");
+		var query = new Parse.Query(Politico);
+		query.equalTo("tipo", params.casa);
+		query.limit(parseInt(params.limit));
+		query.skip(parseInt(params.pg));
+		if(params.fullName){
+			query.equalTo("nome", params.fullName);
+			query.startsWith("nome", params.firstName);
+		}			
+		else{
+			if(params.uf){
+				query.equalTo("uf", params.uf);
+			}
+		}
+		query.find({
+		  success: function(objects) {
+			  var ret = [];
+			  var json = {};
+			  if(objects.length === 0){		
+				
+				json.success = false;
+				json.message = "Não encontrei nenhum parlamentar com esse nome";
+				
+				
+			  }else{
+				// Successfully retrieved the object.										
+				json.success = true;
+				json.objects = objects;
+				
+			  }
+			  ret.push(json);
+			  callback( ret);
+			
+		  },
+		  error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		  }
+		});
+	},
+	
+	/*
+	get congressman spending´s rank 
+	params 
+		uf - state of congressman
+		siglaPartido - parties
+		limit - quantity of return´s itens
+	*/
+	getRanking: function(params, callback){
+		var Politico = Parse.Object.extend("Politico");
+		var query = new Parse.Query(Politico);
+		query.equalTo("tipo", "c");
+		query.limit(params.limit);
+		if(params.uf)
+			query.equalTo("uf", params.uf);
+		if(params.siglaPartido){
+			if(params.siglaPartido === "PCDOB")
+				params.siglaPartido = "PCdoB";
+			query.equalTo("siglaPartido", params.siglaPartido);
+		}
+		query.descending("gastos");
+		query.find({
+		  success: function(objects) {
+			// Successfully retrieved the object.
+			//var cards = [];
+			var mensagem = "Ranking dos "+params.limit+" que mais gastaram:\n"
+			for (var i = 0; i < objects.length; i++) {
+				var object = objects[i];
+				var formattedNumber = format({prefix: 'R$ ',integerSeparator : '.', decimal: ',', round : 2})
+					(object.get("gastos"), {noSeparator: false});
+				mensagem = mensagem + (i+1)+"º "+object.get("nome")+" ("+object.get("siglaPartido")+"-"+
+										object.get("uf")+") "+formattedNumber+"\n";
+			}
+			var ret = [];
+			ret.push(mensagem);
+			callback( ret);
+		  },
+		  error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		  }
+		});
+	},
+	
+	 /*
+		Get projects filter by keywords
+		params: keywords
+	 */
+	
+	searchProjects : function (params, callback){
+		var Politico = Parse.Object.extend("Proposicao");
+		var query = new Parse.Query(Politico);
+		query.containsAll("words", params.keys);	
+		query.limit(10);	
+		query.find({
+		  success: function(objects) {
+			var cards = [];
+			if(objects.length > 0){
+				  // Successfully retrieved the object.
+					for (var i = 0; i < objects.length; i++) {
+						  var object = objects[i];
+						  cards.push(Monitora.buildElementProject(object));
+					}
+					
+					//console.log(cards);
+					
+			  }
+			  callback(cards);
+			
+		  },
+		  error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		  }
+		});
+	},
+	
+	/*
+		 Get congressman projects
+		params: politicoId, nome
+	 */
+	
+	getProjects : function (params, callback){		
+		
+		//var json = params[0];
+		var Proposicao = Parse.Object.extend("Proposicao");
+		var query = new Parse.Query(Proposicao);
+		query.equalTo("autor", "Politico$"+params.politicoId);	
+		query.limit(10);	
+		query.find({
+		  success: function(objects) {
+			// Successfully retrieved the object.
+			var cards = [];
+			for (var i = 0; i < objects.length; i++) {
+				var object = objects[i];
+				cards.push(Monitora.buildElementProject(object));
+			}
+			callback(cards);
+			
+		  },
+		  error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		  }
+		});		
+	},
+	
+	/*
+		 Get congressman´s spending
+		params: politicoId, nome
+	 */
+	
+	getGastos : function (params, callback){		
+		
+		var json = params[0];
+		var gender = Monitora.getGender(json.gender);
+		var CotaPorAno = Parse.Object.extend("CotaPorAno");
+		var query = new Parse.Query(CotaPorAno);
+		query.equalTo("politico", "Politico$"+json.politicoId);	
+		query.find({
+		  success: function(objects) {
+			// Successfully retrieved the object.
+			// Successfully retrieved the object.
+			var mensagem = gender.toUpperCase()+" deputad"+gender+" "+json.nome+" gastou:\n";
+			//console.log(objects.length);
+			for (var i = 0; i < objects.length; i++) {
+				  var object = objects[i];
+				  var formattedNumber = format({prefix: 'R$ ',integerSeparator : '.', decimal: ',', round : 2})
+					(object.get("total"), {noSeparator: false});
+				 mensagem = mensagem + "Ano "+object.get("ano") +": "+formattedNumber+"\n";
+			}
+			var ret = [];
+			ret.push(mensagem);
+			callback( ret);
+			
+		  },
+		  error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		  }
+		});		
+	},
+	
+	/*
+		 Get congressman´s spending by categories
+		params: politicoId, nome,limit
+	 */
+	
+	getSpendingCategories : function (params, callback){		
+		
+		
+		var gender = Monitora.getGender(params.gender);
+		var CotaXCategoria = Parse.Object.extend("CotaXCategoria");
+		var query = new Parse.Query(CotaXCategoria);
+		query.equalTo("politico", "Politico$"+params.politicoId);	
+		query.limit(params.limit);
+		query.descending("total");
+		query.find({
+		  success: function(objects) {
+			  //console.log(objects);
+			// Successfully retrieved the object.
+			var mensagem = "Gastou mais em: \n";
+			//console.log(objects.length);
+			for (var i = 0; i < objects.length; i++) {
+				  var object = objects[i];
+				  var formattedNumber = format({prefix: 'R$ ',integerSeparator : '.', decimal: ',', round : 2})
+					(object.get("total"), {noSeparator: false});
+					
+				 mensagem = mensagem + "Ano "+object.get("ano") +": "+formattedNumber+" em "+
+				 Monitora.cutCategory(object.get("tpCota"))+"\n";
+			}
+			
+			//if(message.length > 320){
+			//	message = message.substring(0, 319)					
+			//}
+			var ret = [];
+			ret.push(mensagem);
+			callback( ret);
+			
+		  },
+		  error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		  }
+		});		
+	},
+	
+	
+	/*
+		Get congressman´s presence
+		params: politicoId, nome
+	 */
+	
+	getPresenca : function (params, callback){		
+		
+		var json = params[0];
+		var gender = Monitora.getGender(json.gender);
+		var Presenca = Parse.Object.extend("Presenca");
+		var query = new Parse.Query(Presenca);
+		query.equalTo("politico", "Politico$"+json.politicoId);	
+		//console.log(qPolitico);
+		query.find({
+		  success: function(objects) {
+			// Successfully retrieved the object.
+			
+			
+			var mensagem = "Presença d"+gender+" deputad"+gender+" "+json.nome+" :\n";
+			
+			for (var i = 0; i < objects.length; i++) {
+				  var object = objects[i];
+				 mensagem = mensagem + "Ano: "+object.get("nr_ano") +"\n     ->Faltas "+
+				 (object.get("nr_ausencia_justificada")+object.get("nr_ausencia_nao_justificada"))+"\n"+
+				 "     ->Presença:"+object.get("nr_presenca")+"\n";
+			}
+			//console.log(mensagem);
+			var ret = [];
+			ret.push(mensagem);
+			callback( ret);
+			
+		  },
+		  error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		  }
+		});		
+	},
+	
+	/* 
+		get congressman´s gender 
+		can be (o) -> male
+		or (a) -> female
+	*/
+	
+	getGender: function (gender){
+		if(gender === "masculino")
+			return "o";
+		return "a";
+	},
+	
+	/* 
+		get congressman´s gender 
+		can be (o) -> male
+		or (a) -> female
+	*/
+	
+	cutCategory: function (cat){
+		cat = cat.replace("MANUTENÇÃO DE ESCRITÓRIO DE APOIO À ATIVIDADE PARLAMENTAR",'Manutenção de escritório');
+		cat = cat.replace("CONSULTORIAS PESQUISAS E TRABALHOS TÉCNICOS",'Consultorias, pesquisas e trab. técn.');
+		cat = cat.replace("DIVULGAÇÃO DA ATIVIDADE PARLAMENTAR",'Divulgação de ativid. parlam.');
+		return cat;
+	}
+};
+
+module.exports = Monitora;
---
+++ rest/app.js
@@ -0,0 +1,46 @@
+ 'use strict';
+
+const 
+  bodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),  
+  request = require('request');
+  
+var Parse = require('parse/node');
+var Monitora = require('./Monitora');
+
+Parse.initialize(config.get('parse_id_app'), config.get('parse_js_key'));
+Parse.serverURL = config.get('parse_url');
+
+var app = express();
+app.set('port',  config.get('port'));
+app.set('view engine', 'ejs');
+//app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
+
+// Serve the Parse API on the /parse URL prefix
+//var parseApiPath = '/monitorarest';
+//app.use(parseApiPath, api);
+
+
+app.get('/monitorarest/getPoliticos/:casa/:ativo/:limit', function(req, res) {	
+	var pg = req.param('pg'); 
+	req.params.pg=pg;
+	Monitora.getPolitico(req.params, function(ret){	
+		res.send(ret);
+		
+	});	
+});
+
+
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid 
+// certificate authority.
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
